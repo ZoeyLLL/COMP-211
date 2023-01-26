f3d703715df66abdeb2cab72c9979278ae23c0a2
ZoeyLu <112651748+ZoeyLLL@users.noreply.github.com>
2023-01-26 09:40:28 -0500

    "Hello, C!"

---
+++ lab-00-ZoeyLLL/0-hello-world/hello.c
@@ -0,0 +1,12 @@
+//PID: 730323145
+//I pledge the COMP 211 honor code. 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+    printf("Hello, world.\n");
+    printf("Welcome to C!\n"); 
+    return EXIT_SUCCESS;
+}
+
